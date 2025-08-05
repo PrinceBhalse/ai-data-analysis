b1b087ab610d278777cba82a9c460820efeb4c30
Prince Bhalse <80117253+PrinceBhalse@users.noreply.github.com>
2025-08-06 04:16:35 +0530

    Delete src/components/ChartErrorComponent.tsx

--- src/components/ChartErrorComponent.tsx
+++
@@ -1,7 +0,0 @@
-function ChartErrorComponent() {
-  return (
-    <div className="bg-red-50 border border-red-200 p-4 rounded text-red-800">
-      Failed to render chart. Try refreshing the page.
-    </div>
-  );
-}
